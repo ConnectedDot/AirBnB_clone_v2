10c374cb6472d880ad7bbeaac3c7b6ec9e9b1bc0
Theophilus Samuel <36473098+ConnectedDot@users.noreply.github.com>
2024-02-04 09:12:05 +0100

    Deploy static script

--- transfer.sh
+++ transfer.sh
@@ -3,16 +3,16 @@
 #
 # Variables
 path="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-filename=$(basename "$BASH_SOURCE")
+filename=$(basename "$0")
 brown='\e[0;33m'
 reset='\033[0m'
 file="$1"
 ip="$2"
-user="ubuntu"
-keys="~/.ssh/id_rsa"
+user="$3"
+keys="$4"
 
-if [ $# -eq 2 ]; then
-  scp -i "$keys" -o StrictHostKeyChecking=no "$path/$file" "$user@$ip:/home/$user/"
+if [ $# -eq 4 ]; then
+  scp -r -i "$keys" -o StrictHostKeyChecking=no "$file" "$user@$ip:/home/$user/"
 else
   echo -e "Usage: ${brown}$filename PATH_TO_FILE IP USERNAME PATH_TO_SSH_KEY${reset}"
 fi
